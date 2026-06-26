235e6f1f88915da95aa7b0fd627f0143fa5e4399
Gus <135130384+06gusmog@users.noreply.github.com>
2026-06-27 01:14:47 +0200

    Final FINAL polish sesh

--- TitleScreen.cs
+++ TitleScreen.cs
@@ -6,6 +6,10 @@ public partial class TitleScreen : Control
 	// Called when the node enters the scene tree for the first time.
 	public void _on_start_game()
 	{
-		GetParent<Menu>().switch_menu("Main");
+		Global.Instance.wave = 0;
+		Global.Instance.current_items = [];
+		Global.Instance.space_ability = "None";
+		Global.Instance.shift_ability = "None";
+		GetParent<Menu>().fade_out("Cutscenes/first_blorbo_encounter.tscn");
 	}
 }
