160476a6bf75160b34b3c31efb1f2d9390fdd862
Gus-does-stuff <mogren.gustav@gmail.com>
2026-06-26 15:35:34 +0200

    Wow I should have texted it before pushing my fix

--- MainMenu.cs
+++ MainMenu.cs
@@ -5,7 +5,7 @@ public partial class MainMenu : Control
 {
 	public void _on_run_it_button_up()
 	{
-		GetParent<Menu>().fade_out("res://Cutscenes/FirstBlorboEncounter.cs");
+		GetParent<Menu>().fade_out("Cutscenes/first_blorbo_encounter.tscn");
 	}
 	public void _on_arena_button_up()
 	{
